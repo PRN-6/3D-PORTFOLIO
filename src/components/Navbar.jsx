386cc0beee072f71871dece61865e8c2406bb279
prinsonnazareth2@gmail.com <prinsonnazareth2@gmail.com>
2025-12-06 12:01:56 +0530

    updating

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Link } from 'react-router'
 import { FaSun, FaMoon } from 'react-icons/fa';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 const Navbar = () => {
+    const navLink = useRef(null)
+
+    useGSAP(() => {
+        gsap.from(navLink.current, {
+            y:50, opacity:0, duration:1, ease:'circ.in'
+        })
+    })
   return (
     <header className='md' >
         <div className='mx-auto h-16 p-4 bg-black'>
             <div className='flex items-center justify-between'>
-                <div className='flex w-full justify-between sm:justify-end gap-6 px-2 sm:px-6 text-sm '>
+                <div ref={navLink} className='flex w-full justify-between sm:justify-end gap-6 px-2 sm:px-6 text-sm '>
                     <Link to={"/about"}>
                         <h3>
                             About
